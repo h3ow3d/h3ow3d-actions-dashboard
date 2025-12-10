ee6614d35046aa87d3de2ce00f604445ea9fc9f2
h3ow3d <holdens.uk@proton.me>
2025-12-10 21:28:13 +0000

    fix: removing duplicate backend

--- infra/versions.tf
+++ infra/versions.tf
@@ -7,15 +7,6 @@ terraform {
       version = "~> 6.0"
     }
   }
-
-  backend "s3" {
-    # Configure via backend config file or environment variables
-    # bucket         = "your-terraform-state-bucket"
-    # key            = "actions-dashboard/terraform.tfstate"
-    # region         = "eu-west-2"
-    # encrypt        = true
-    # dynamodb_table = "terraform-locks"
-  }
 }
 
 provider "aws" {
